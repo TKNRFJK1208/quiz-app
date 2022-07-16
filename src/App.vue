9594cdc618b980dc7b0f5991e721a23b21cc2edc
takanori <takanori@goopter.com>
2022-07-15 21:29:41 -0700

    added functions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,111 @@
 <template>
+  <main class="app">
+    <h1>The Quiz</h1>
+
+    <section class="quiz" v-if="!quizCompleted">
+      <div class="quiz-info">
+        <span class="question">{{ getCurrentQuestion.question }}</span>
+        <span class="score">Score {{ score }}/{{ questions.length }}</span>
+      </div>
+
+      <div class="options">
+        <label v-for="(option, index) in getCurrentQuestion.options" :key="index" :for="'option' + index" :class="`option ${getCurrentQuestion.selected == index
+        ? index == getCurrentQuestion.answer
+          ? 'correct'
+          : 'wrong'
+        : ''
+        } ${getCurrentQuestion.selected != null &&
+          index != getCurrentQuestion.selected
+          ? 'disabled'
+          : ''
+        }`">
+          <input type="radio" :id="'option' + index" :name="getCurrentQuestion.index" :value="index"
+            v-model="getCurrentQuestion.selected" :disabled="getCurrentQuestion.selected" @change="SetAnswer" />
+          <span>{{ option }}</span>
+        </label>
+      </div>
+
+      <button @click="NextQuestion" :disabled="!getCurrentQuestion.selected">
+        {{
+            getCurrentQuestion.index == questions.length - 1
+              ? 'Finish'
+              : getCurrentQuestion.selected == null
+                ? 'Select an option'
+                : 'Next question'
+        }}
+      </button>
+    </section>
+
+    <section v-else>
+      <h2>You have finished the quiz!</h2>
+      <p>Your score is {{ score }}/{{ questions.length }}</p>
+    </section>
+  </main>
 </template>
 
-<script>
+<script setup>
+import { ref, computed } from 'vue'
+const questions = ref([
+  {
+    question: 'What is Vue?',
+    answer: 0,
+    options: [
+      'A framework',
+      'A library',
+      'A type of hat'
+    ],
+    selected: null
+  },
+  {
+    question: 'What is Vuex used for?',
+    answer: 2,
+    options: [
+      'Eating a delicious snack',
+      'Viewing things',
+      'State management'
+    ],
+    selected: null
+  },
+  {
+    question: 'What is Vue Router?',
+    answer: 1,
+    options: [
+      'An ice cream maker',
+      'A routing library for Vue',
+      'Burger sauce'
+    ],
+    selected: null
+  }
+])
+const quizCompleted = ref(false)
+const currentQuestion = ref(0)
+const score = computed(() => {
+  let value = 0
+  questions.value.map(q => {
+    if (q.selected != null && q.answer == q.selected) {
+      console.log('correct');
+      value++
+    }
+  })
+  return value
+})
+const getCurrentQuestion = computed(() => {
+  let question = questions.value[currentQuestion.value]
+  question.index = currentQuestion.value
+  return question
+})
+const SetAnswer = (e) => {
+  questions.value[currentQuestion.value].selected = e.target.value
+  e.target.value = null
+}
+const NextQuestion = () => {
+  if (currentQuestion.value < questions.value.length - 1) {
+    currentQuestion.value++
+    return
+  }
+
+  quizCompleted.value = true
+}
 </script>
 
 <style>
